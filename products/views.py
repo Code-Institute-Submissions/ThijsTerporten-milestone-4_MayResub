b11574e08145dafda8c817e5b5d70f3842e527bf
ThijsTerporten <twpterporten@gmail.com>
2022-03-06 18:49:23 +0000

    Add view for full product details Add product_detail.html and boilerplate html

--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 """ Views for the Products App """
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here
@@ -15,3 +15,14 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show product details """
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
