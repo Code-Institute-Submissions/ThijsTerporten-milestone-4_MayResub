8bd17246a222d7a3ba877b6bc59cdafbd6876031
ThijsTerporten <twpterporten@gmail.com>
2022-03-14 20:38:07 +0000

    Add functions for grand_total saving ordernumber and orderline item Run migrations

--- checkout/models.py
+++ checkout/models.py
@@ -27,6 +27,31 @@ class Order(models.Model):
     grand_total = models.DecimalField(
         max_digits=10, decimal_places=2, null=False, default=0)
 
+    def _generate_order_number(self):
+        """ Generate a random unique order number """
+        return uuid.uuid4().hex.upper()
+
+    def save(self, *args, **kwargs):
+        """ Override the original save method to set the order number """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def update_total(self):
+        """ Update grand total every time a line item is added """
+        self.order_total = self.lineitems.aggregate(
+            Sum('lineitem_total'))['lineitem_total__sum']
+        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+        else:
+            self.delivery_cost = 0
+        self.grand_total = self.order_total + self.delivery_cost
+        self.save()
+
+    def __str__(self):
+        """ Return the order number """
+        return self.order_number
+
 
 class OrderLineItem(models.Model):
     """ Class for orderline items """
@@ -48,3 +73,15 @@ class OrderLineItem(models.Model):
         null=False,
         blank=False,
         editable=False)
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the lineitem total
+        and update the order total
+        """
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        """ Return SKU on order """
+        return f'SKU {self.product.sku} on order {self.order.order_number}'
