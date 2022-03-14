e0f8f29984861f5b69b27463175d6f9d9f8e76ea
ThijsTerporten <twpterporten@gmail.com>
2022-03-14 15:49:43 +0000

    Add success message and toast for loop

--- bag/views.py
+++ bag/views.py
@@ -1,7 +1,8 @@
 """ Views for the Shopping bag App """
 
 from django.shortcuts import render, redirect, reverse, HttpResponse
-
+from django.contrib import messages
+from products.models import Product
 # Create your views here
 
 
@@ -16,14 +17,14 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-
+    product = Product.objects.get(pk=item_id)
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
-
+        messages.success(request, f'Added {product.name} to your bag')
     request.session['bag'] = bag
     return redirect(redirect_url)
 
