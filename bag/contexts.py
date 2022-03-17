41dc419617b92cc108cac406ecbcb9fd9ce26f28
ThijsTerporten <twpterporten@gmail.com>
2022-03-17 21:37:14 +0000

    Fix typo

--- bag/contexts.py
+++ bag/contexts.py
@@ -38,7 +38,7 @@ def bag_contents(request):
         'product_count': product_count,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
-        'free_delivery_threshol;d': settings.FREE_DELIVERY_THRESHOLD,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
     }
 
