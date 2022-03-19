feedee4009e2acbbdc21e45a639e687e925006e6
ThijsTerporten <twpterporten@gmail.com>
2022-03-19 02:28:34 +0000

    Add deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn comix.wsgi:application
--- comix/settings.py
+++ comix/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-77w1h$^qb93wuc9a55zwp9yh=b+ko4gu7h8q=6t)))r@_fc=6d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['thijsterporten-milestone-4.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -123,12 +123,17 @@ WSGI_APPLICATION = 'comix.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.pase(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
