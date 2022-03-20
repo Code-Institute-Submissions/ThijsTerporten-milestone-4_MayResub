da1feb4a9c6194414fd77bb3ad64ef75f75c6f28
ThijsTerporten <twpterporten@gmail.com>
2022-03-20 00:48:06 +0000

    Add pep8 formatting

--- products/forms.py
+++ products/forms.py
@@ -11,9 +11,12 @@ class ProductForm(forms.ModelForm):
         """ Meta Class for products and categories """
         model = Product
         fields = '__all__'
-    
-    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
+
+    image = forms.ImageField(
+        label='Image', required=False, widget=CustomClearableFileInput)
+
     def __init__(self, *args, **kwargs):
+
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
