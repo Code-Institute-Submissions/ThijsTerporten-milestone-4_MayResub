76f98a49451d169f639e388b07c6ba7977622136
ThijsTerporten <twpterporten@gmail.com>
2022-03-20 00:05:26 +0000

    Set debug to true

--- comix/settings.py
+++ comix/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')  # noqa: E501
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
-
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 ALLOWED_HOSTS = ['thijsterporten-milestone-4.herokuapp.com', 'localhost']
 
 
