006fbe2660bcbbc28a3db5ae5e8501e4dabdfdad
ThijsTerporten <twpterporten@gmail.com>
2022-05-11 21:25:52 +0000

    Set debug to False for final deployment before submission

--- comix/settings.py
+++ comix/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')  # noqa: E501
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 ALLOWED_HOSTS = ['thijsterporten-milestone-4.herokuapp.com', 'localhost']
 
 
